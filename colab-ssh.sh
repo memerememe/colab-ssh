568d10f4845c97ad18fa784bcad6d9a59d268c50
memerememe <47179281+memerememe@users.noreply.github.com>
2021-04-02 15:33:34 +0800

    Update colab-ssh.sh

--- colab-ssh.sh
+++ colab-ssh.sh
@@ -9,17 +9,17 @@ if [[ "${PASSWORD}" == "" ]]
         echo "[USAGE] ./colab-ssh.sh [password]"
 else
     ################################## SSH SETTINGS ##############################################
-    cp $BASEDIR/sshd_config /etc/ssh/
-    mkdir -p /var/run/sshd
-    /usr/sbin/sshd -D &
+    sudo cp $BASEDIR/sshd_config /etc/ssh/
+    sudo mkdir -p /var/run/sshd
+    sudo /usr/sbin/sshd -D &
     echo "Sshd daemon start..."
 
     ################################## SYSTEM SETTINGS ##############################################
-    cp $BASEDIR/.bashrc /root/
+    sudo cp $BASEDIR/.bashrc /root/
     echo "Update .basrhc!"
-    apt-get -y -qq install htop vim
+    sudo apt-get -y -qq install htop vim
     echo "Install... vim, htop "
-    echo root:${PASSWORD} | chpasswd
+    echo root:${PASSWORD} | sudo chpasswd
 
     ################################## TUNNEL SETTINGS ##############################################
     # Tunnel is activating
@@ -28,8 +28,8 @@ else
     if [ -n "$pid" ]; then
 	    kill $pid
     fi
-    rm -rf ~/.tunnel.log
-    nohup $BASEDIR/tunnel-cli --tcp 22 >~/.tunnel.log 2>/dev/null &
+    sudo rm -rf ~/.tunnel.log
+    sudo nohup $BASEDIR/tunnel-cli --tcp 22 >~/.tunnel.log 2>/dev/null &
     sleep 1
     cat ~/.tunnel.log
 fi
